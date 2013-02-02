ec72c6ab811eb3f52cbfbac98b3c9f610f767eac
Кирилл <ars@macbook-pro-ars.(none)>
2013-02-02 22:56:50 +0600

    Initial Commit

---
+++ KKToolKit/KKToolKit.h
@@ -0,0 +1,13 @@
+//
+//  KKToolKit.h
+//  KKToolKit
+//
+//  Created by Кирилл on 02.02.13.
+//  Copyright (c) 2013 Kirill Kunst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKToolKit : NSObject
+
+@end
---
+++ KKToolKit/KKToolKit.m
@@ -0,0 +1,13 @@
+//
+//  KKToolKit.m
+//  KKToolKit
+//
+//  Created by Кирилл on 02.02.13.
+//  Copyright (c) 2013 Kirill Kunst. All rights reserved.
+//
+
+#import "KKToolKit.h"
+
+@implementation KKToolKit
+
+@end
